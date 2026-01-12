835e1413cf7021342e94d9d4f90729853c2d35b7
Winston Wong <winstonwong0@gmail.com>
2026-01-11 20:43:23 -0600

    Enabled frontend filtering in "page.tsx"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import TaskTable from "../components/TaskTable";
 import Task from "../components/Task";
 import { useTasks } from "../hooks/useTasks";
@@ -8,6 +9,12 @@ import AddItem from "../components/AddItem";
 
 export default function Home() {
   const { tasks, loading, error } = useTasks();
+  const [showCompletedOnly, setShowCompletedOnly] = useState(false);
+
+  // Filter tasks on the front end
+  const visibleTasks = showCompletedOnly
+    ? tasks?.filter((task) => task.isDone)
+    : tasks;
 
   return (
     <>
@@ -15,13 +22,26 @@ export default function Home() {
       {loading && <div>Loading...</div>}
       {!loading && (
         <>
+          {/* Filter toggle */}
+          <div className="flex justify -center mb-4 gap-2">
+            <input
+              type="checkbox"
+              checked={showCompletedOnly}
+              onChange={() => setShowCompletedOnly((prev) => !prev)}
+            />
+            <span>Show completed only</span>
+          </div>
+
+          {/* Task list */}
           <TaskTable>
-            {tasks!.map((task: TaskData) => (
+            {visibleTasks!.map((task: TaskData) => (
               <tr key={task.id}>
                 <Task task={task} />
               </tr>
             ))}
           </TaskTable>
+
+          {/* Add new Task */}
           <div className="flex justify-center mt-4">
             <AddItem />
           </div>
